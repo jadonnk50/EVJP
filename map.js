06628a6800f109096c759346897f79776ce028d2
jadonnk50 <77672052+jadonnk50@users.noreply.github.com>
2022-03-19 00:37:00 +0000

    Basic Map done

---
+++ map.js
@@ -0,0 +1,10 @@
+var map = L.map('map').setView([57.11973723727868, -2.13965135269459], 17);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiamFkb25uazUwIiwiYSI6ImNsMHgzb3IzcTFnaGIzZG41OHJpbWNhd3YifQ.iRmUKqleOXpk27nXvL-zkA'
+}).addTo(map);
