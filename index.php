b5977d88cbca84fe1f3040dda48f33f506133470
jadonnk50 <77672052+jadonnk50@users.noreply.github.com>
2022-03-23 00:48:43 +0000

    First week landmark

--- index.php
+++ index.php
@@ -6,40 +6,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EV Journey Planner</title>
     
-<!-- leaflet css -->
+  <!-- leaflet css -->
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin=""/>
-    
-    <link rel="stylesheet" href="style.css"/>
+ 
     <!-- <link rel="stylesheet" href="./signup.css"> -->
+    <link rel="stylesheet" href="style.css"/>
     <link rel="stylesheet" href="navbar.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
+
 <body>
+
+  <!-- Top Navigation Bar -->
     <div class="topnav" id="myTopnav">
-        <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Plan Journey</span>
-        <a href="#home">EVRP            </a>
-        <a href="#news">News</a>
-        <a href="#contact">Contact</a>
         <a href="#about">About</a>
-        <a href="signup.php" class="active">Sign Up</a>
-        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-          <i class="fa fa-bars"></i>
-        </a>
+        <a href="#contact">Contact</a>
+        <a href="signup.php">Sign Up</a>
+        <a href="signup.php">Log In</a>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()"><i class="fa fa-bars"></i></a>
+        <button onclick="openNav()"> Plan Journey</button>
+    </div>
+
+  <!-- input field -->
+    <div id="mySidenav" class="sidenav">
+      <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>  
+      
+      <div class="journeydetails">
+        <form action="/action_page.php">
+          <input type="text" id="start" name="firstname" placeholder="The starting point of your trip">
+          <input type="text" id="finish" name="lastname" placeholder="Destination">
+          <input type="text" id="battery" name="firstname" placeholder="Departure Charge %">
+          <input type="text" id="lname" name="lastname" placeholder="Vehicle Range">      
+          <select id="charger" name="Charger Type">
+            <option value="rapid">Rapid Charge</option>
+            <option value="fast">Fast Charge</option>
+            <option value="slow">Slow Charge</option>
+          </select>
+
+          <input type="submit" value="Start Plan">
+        </form>
       </div>
-   <div id="mySidenav" class="sidenav">
-          <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-          <a href="#">About</a>
-          <a href="#">Services</a>
-          <a href="#">Clients</a>
-          <a href="#">Contact</a>
-   </div>
 
-   <div id="main">
-    <div id="map"></div>
-   </div>   
+    </div>
+
+  <!-- Map container -->
+    <div id="main">
+        <div id="map"></div>
+    </div>
+
+
     
-
-
 </body>
 </html>
 
@@ -47,7 +63,8 @@
 <!-- leaflet js -->
 <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin="">
 </script>
+
+<!-- other scripts -->
 <script src="map.js"></script>
 <script src="./sidenav.js"></script>
 <script src="signup.js"></script>
-<!-- <script src="./chargers/chargepoint.js"></script> -->
