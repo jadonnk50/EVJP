380f7527ae037f4e0ee2a122ef2bf4df6dcb7a44
jadonnk50 <77672052+jadonnk50@users.noreply.github.com>
2022-03-25 18:20:58 +0000

    json

--- config.php
+++ config.php
@@ -6,6 +6,7 @@ $db_name = "evrp";
 try{
     $db = new PDO('mysql:host=localhost; dbname='. $db_name.  ';charset=utf8', $db_user, $db_password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
