db35b598ff8b8f0b9594bd293d02ba145cbca2e5
jadonnk50 <77672052+jadonnk50@users.noreply.github.com>
2022-04-01 20:30:09 +0100

    COMMIT

--- map.js
+++ map.js
@@ -1,63 +1,62 @@
-var map = L.map('map').setView([57.11973723727868, -2.13965135269459], 17);
+var jstart;
+var jfinish;  
+var SOC; 
+var EVrange;
+var charger;
+var safeDrivableDistance;
+var lat;
+var long;
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiamFkb25uazUwIiwiYSI6ImNsMHgzb3IzcTFnaGIzZG41OHJpbWNhd3YifQ.iRmUKqleOXpk27nXvL-zkA'
-}).addTo(map);
+  // get Journey planner Form
+  const journeyForm = document.getElementById('journey');
+
+  // add listener to the form
+  journeyForm.addEventListener('submit', JourneyForm)
+
 
 //getting the form data
-async function JourneyForm(event){
+function JourneyForm(event){
     event.preventDefault();
     console.log('form submited');
     
 
     //getting the form data
-    var jstart   = document.getElementById('journeystart').value;
-    var jfinish  = document.getElementById('journeyfinish').value;
-    var SOC      = document.getElementById("evSOC").value;
-    var EVrange  = document.getElementById("EVrange").value;
-    var charger  = document.getElementById("charger").value;
+    jstart   = document.getElementById('journeystart').value;
+    jfinish  = document.getElementById('journeyfinish').value;
+    SOC      = document.getElementById("evSOC").value;
+    EVrange  = document.getElementById("EVrange").value;
+    charger  = document.getElementById("charger").value;
+    
     console.log(jstart + 'jstart')
     console.log(jfinish + 'jfinish')
     console.log(SOC + 'SOC')
     console.log(EVrange + 'EVrange')
 
     
-     //manipulation of the form data to calculation
-    SOC = SOC/100
-    console.log(SOC + ' %')
+     //manipulation of form data.
+    SOC = SOC/100;
+    console.log(SOC + ' %');
 
     //Subtract 10% battery power from the CBP for allowance.
-    SOC = SOC-0.1
-    console.log(SOC + ' % is safe SOC')
+    SOC = SOC-0.1;
+    console.log(SOC + ' % is safe SOC');
 
     //Using the SOC minus 10%, calculate the safe Drivable Range of the EV (DR)
-    var safeDrivableDistance = EVrange*SOC
+    safeDrivableDistance = EVrange*SOC
     console.log(safeDrivableDistance +' miles')
 
-    var location = document.getElementById('journeystart').value;
-    var geometry = geocode(location);
-    
     //locate the charging station by calling the chargestation function.
-    var distance = safeDrivableDistance;
-    // var geometry = (long);
-    setTimeout(console.log(geometry), 5000);
     chargestations(distance, geometry);
 }
 
-  // get Journey planner Form
-const journeyForm = document.getElementById('journey');
-
-// add listener to the form
-journeyForm.addEventListener('submit', JourneyForm)
-
-
 function chargestations(distance, longitude, latitude){
 
+  var location = jstart;
+  geocode(location);
+
+  var distance = safeDrivableDistance;
+  var geometry = (long);
+  setTimeout(console.log(geometry), 5000);
   
             axios.get('https://chargepoints.dft.gov.uk/api/retrieve/registry/format/json', {
                 params:{
@@ -99,11 +98,9 @@ function geocode(location){
           console.log(lat);
           console.log(long);
 
-          return long;
+          return [lat, long];
         })
         .catch(function(error){
           console.log(error);
         });
       }
-
-        
